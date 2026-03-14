a9d7369136e8f6adc3f4420e0a1b73b17889277d
ashamallasamudra <ashamallasamudra56@gmail.com>
2026-03-14 15:50:56 +0530

    code from v1 tov5

---
+++ video_1.py
@@ -0,0 +1,84 @@
+
+#v2
+# #Comments
+#comment1
+#comment2
+#comment 3
+
+"""helle i am kiran"""
+''' hey kiran'''
+print(1+4+3)
+'pen'
+"pencil"
+
+#v3
+#variables and datatypes
+a=10
+b=20
+c="asha"
+d="ture"
+print(a)
+print(b)
+print(c)
+print(d)
+
+a,b,c=10,20,'asha'
+print(a,b,c)
+
+a,b,c=10,10,10
+print(a,b,c)
+a=b=c=10
+print(a,b,c)
+
+
+x=1
+y=2
+y,x=x,y
+print(x,y)
+print(x)
+
+#v4
+#Datatypes
+a=10
+b=20.1
+c="asha"
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#Concatination
+print(1+1)
+print(1+1.1)
+print(1.1+1.1)
+print("go"+"come")
+print(True+1)
+print(False+2)
+print(False+1.1)
+
+
+#Invalid
+#print("go"+8)
+#print(True+"go")
+#v5
+# swapping
+x=1
+y=2
+print("Before swapping:",x,y)
+y,x=x,y
+print("After swapping:",x,y)
+
+
+#Redeclaration
+a=1
+print(a)
+
+a=20
+print(a)
+
+#Delete variable
+"""a=2
+print(a)
+del a
+print(a)"""
